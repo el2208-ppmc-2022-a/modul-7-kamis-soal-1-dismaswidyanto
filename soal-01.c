a31e82887a76acc2e84e176d963a63a989ab2893
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-04-05 10:35:03 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -12,12 +12,16 @@
 #include <stdlib.h>
 
 // tipe data simulasi stack
-...
+typedef struct stack {
+	int data;
+	struct stack *next;
+} stack;
  
-// deklarasi variabel awal 
-int pop( ... );
-void push( ... , ... );
-void printStack(stack *x); //silakan sesuaikan tipe data stack dengan yang anda gunakan
+// deklarasi variabel awal
+int pop(stack **x);
+void push(stack **x, int data);
+void printStack(stack *x);
+void debuggerPrint(stack *input, stack *output);
 int coprime(int x, int y);
 int lcm(int x, int y);
 int gcd(int x, int y);
@@ -25,44 +29,104 @@ int gcd(int x, int y);
 // fungsi utama
 int main(){ 
 	// deklarasi stack input dan output
-	...
+	stack *input = NULL; 
+	stack *output = NULL;  
+	
+	int userinput = 0; 
+	int counter = 0; 
+	int isFirstIteration = 0;
+
+	// variabel temporary untuk menyimpan data yang dipop dari struct
+	int pop_out;
+	int pop_inp;
+	int pop_out_1;
+	int pop_out_2;
 
 	// while loop untuk mengambil input angka dari user
 	// loop berhenti bila user memberi input 0
-	printf("Input angka ke-%d: ", ...);
-	scanf("%d", ...);
-
-	printf("\n");
-	 
-	// menampilkan stack input sebelum dikosongkan pada proses komputasi
-	printf("Stack input: ");
-
-	// algoritma komparasi elemen stack
-	...
-	...
+	while (1){ 
+		counter++;
+		printf("Input angka ke-%d: ", counter);
+		scanf("%d", &userinput);
+		
+		if (userinput == 0){
+			printf("\n");
+			break;
+		} 
+		else{
+			push(&input, userinput);
+		} 
+	}
 	
+	// menampilkan stack input sebelum dikosongkan pada proses komputasi  
+	printf("Stack input: ");
+	printStack(input); 
+
+	// while loop untuk melakukan komparasi elemen stack input 
+	// loop berhenti bila stack input sudah kosong
+	while(input!=NULL){ 
+		// iterasi pertama, pop dua elemen input untuk ditinjau
+		if (isFirstIteration == 0){ 
+			pop_out = pop(&input);
+			pop_inp = pop(&input); 
+			
+			// kondisi dimana kedua bilangan koprima. Kedua bilangan tersebut masuk ke stack output
+			if (coprime(pop_inp,pop_out) == 1){ 
+				push(&output, pop_out);
+				push(&output, pop_inp);
+			}
+
+			
+			// kondisi dimana kedua bilangan tidak koprima. KPK dari kedua bilangan tersebut dimasukkan ke stack output
+			else{  
+				int lcmOut = lcm(pop_out,pop_inp);  
+				push(&output, lcmOut);  
+			} 
+			isFirstIteration = 1; 
+		}
+
+		// iterasi kedua dan seterusnya, pop 1 elemen stack input dan 1 elemen stack output untuk ditinjau
+		else{  
+			pop_out = pop(&output);
+			pop_inp = pop(&input);  
+			
+			// bila 2 bilangan tsb koprima, kedua bilangan tersebut masuk ke stack output
+			if (coprime(pop_inp,pop_out) == 1){ 
+				push(&output, pop_out);
+				push(&output, pop_inp); 
+			}
+
+			// kondisi dimana kedua bilangan tidak koprima
+			else{  
+				// push KPK kedua bilangan ke stack output
+				push(&output, lcm(pop_out,pop_inp)); 
+
+				// while loop untuk meninjau apakah KPK yang dipush memiliki koprima dengan elemen sebelumnya pada stack output 
+				while(output->next != NULL){   
+					pop_out_1 = pop(&output);
+					pop_out_2 = pop(&output); 
+					
+					if (coprime(pop_out_1,pop_out_2) == 1){ 
+						push(&output, pop_out_2);
+						push(&output, pop_out_1);
+						break;
+					}
+					else{
+						push(&output, lcm(pop_out_1, pop_out_2));
+					}
+				}
+			}
+
+		} 
+	}
 	
 	// print hasil algoritma
 	printf("Stack output: ");
-	
-	return 0;
-}
-
-// fungsi push elemen ke stack
-void push( ..., ... ) {
-	// tulis kode anda disini
-	... 
-}
-
-// fungsi untuk pop elemen terakhir dari stack 
-int pop( ... ) {
-	// tulis kode anda disini
-	... 
+	printStack(output);
 }
 
 // fungsi mencari FPB antara bilangan x dan y
 int gcd(int x, int y){      
-	printf("x,y=%d,%d\n", x, y);
 	while(x != y)
 	{
 		if(x > y)
@@ -70,9 +134,6 @@ int gcd(int x, int y){
 		else
 			y -= x;
 	} 
-	
-	int ret = x;
-	return ret;
 } 
 
 // fungsi mencari KPK antara bilangan x dan y
@@ -93,8 +154,26 @@ int coprime(int x, int y){
 	} 
 }
 
+// fungsi push elemen ke stack
+void push(stack **x, int data) {
+	stack *temp = (stack*)malloc(sizeof(stack));
+	temp->data = data;
+	temp->next = *x;
+	*x = temp;
+}
+
+// fungsi untuk pop elemen terakhir dari stack 
+int pop(stack **x) {
+	if (*x == NULL)
+		return 0;
+	stack *temp = *x;
+	int ret = temp->data;
+	*x = (*x)->next;
+	free(temp);
+	return ret;
+}
+
 // fungsi untuk print elemen stack
-//Silakan sesuaikan bentuk data anda dengan yang anda gunakan
 void printStack(stack *x) {
 	stack *temp = x; 
 
@@ -108,4 +187,12 @@ void printStack(stack *x) {
 		temp = temp->next;
 	}
 	printf("%d]\n", temp->data);
+}
+
+// fungsi bantuan untuk keperluan debugging (print stack input dan output)
+void debuggerPrint(stack *input, stack *output){
+	printf("INPUT: ");
+	printStack(input);
+	printf("OUTPUT: ");
+	printStack(output);
 }
